0a713b6face02b9360b326b6330526e670e357f5
MinnullinIrek <irekminn4@gmail.com>
2017-06-06 22:41:18 +0300

    first

---
+++ main.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+class Root(BoxLayout):
+    pass
+
+class ExampleApp(App):
+    def build(self):
+        return Root()
+ExampleApp().run()
